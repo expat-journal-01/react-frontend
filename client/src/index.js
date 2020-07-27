83b1602d8e1baef82bdfa2c5b2c7d8a8dc0dc301
Dominique K Kitchenakow <domikitchen@users.noreply.github.com>
2020-07-27 09:46:10 -0500

    added component files, fixed up the index and renamed login to match the endpoints

--- client/src/index.js
+++ client/src/index.js
@@ -2,14 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
+import thunk from 'redux-thunk';
 import './index.css';
 import App from './App';
 
+import { reducer } from './reducers/index';
 
+const store = createStore(reducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store = {store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
